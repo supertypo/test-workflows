553ad9db4f67e2419f4d66e0991241828f4c21c0
supertypo <suprtypo@pm.me>
2024-12-22 20:30:53 +0100

    Dependency cleanup

--- filler/src/vars/vars.rs
+++ filler/src/vars/vars.rs
@@ -1,16 +1,21 @@
 use kaspa_database::client::client::KaspaDbClient;
-use sqlx::Error;
 
 pub const VAR_KEY_BLOCK_CHECKPOINT: &str = "block_checkpoint";
 pub const VAR_KEY_LEGACY_CHECKPOINT: &str = "vspc_last_start_hash";
 
-pub async fn load_block_checkpoint(database: &KaspaDbClient) -> Result<String, Error> {
+pub async fn load_block_checkpoint(database: &KaspaDbClient) -> Result<String, ()> {
     if let Ok(block_hash) = database.select_var(VAR_KEY_BLOCK_CHECKPOINT).await {
         return Ok(block_hash);
     }
-    database.select_var(VAR_KEY_LEGACY_CHECKPOINT).await
+    if let Ok(block_hash) = database.select_var(VAR_KEY_LEGACY_CHECKPOINT).await {
+        return Ok(block_hash);
+    }
+    Err(())
 }
 
-pub async fn save_checkpoint(block_hash: &String, database: &KaspaDbClient) -> Result<u64, Error> {
-    database.upsert_var(VAR_KEY_BLOCK_CHECKPOINT, block_hash).await
+pub async fn save_checkpoint(block_hash: &String, database: &KaspaDbClient) -> Result<u64, ()> {
+    match database.upsert_var(VAR_KEY_BLOCK_CHECKPOINT, block_hash).await {
+        Ok(rows_affected) => { Ok(rows_affected) }
+        Err(_) => { Err(()) }
+    }
 }
