9e7ff392370ac6c394d493407378e9e89f3d6ee3
supertypo <suprtypo@pm.me>
2024-12-22 20:30:51 +0100

    Implemented removed chain blocks

--- src/virtual_chain/process_virtual_chain.rs
+++ src/virtual_chain/process_virtual_chain.rs
@@ -1,21 +1,22 @@
 extern crate diesel;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
 
 use crossbeam_queue::ArrayQueue;
-use diesel::{BoolExpressionMethods, Connection, ExpressionMethods, insert_into, PgConnection, QueryDsl, RunQueryDsl};
-use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::{Connection, ExpressionMethods, insert_into, PgConnection, QueryDsl, RunQueryDsl};
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 use diesel::result::Error;
 use diesel::upsert::excluded;
 use kaspa_rpc_core::api::rpc::RpcApi;
+use kaspa_rpc_core::RpcHash;
 use kaspa_wrpc_client::KaspaRpcClient;
 use log::{info, trace, warn};
 use tokio::time::sleep;
 
 use crate::database::models::{Block, Transaction};
-use crate::database::schema::{transactions, blocks};
+use crate::database::schema::{blocks, transactions};
 use crate::vars::vars::save_virtual_checkpoint;
 
 pub async fn process_virtual_chain(checkpoint_hash: String,
@@ -88,33 +89,7 @@ pub async fn process_virtual_chain(checkpoint_hash: String,
             }
         }
 
-        if !response.added_chain_block_hashes.is_empty() {
-            info!("Received {} added chain blocks", response.added_chain_block_hashes.len());
-            trace!("Added chain blocks: \n{:#?}", response.added_chain_block_hashes);
-            let mut chain_hashes = response.added_chain_block_hashes.iter().map(|b| b.as_bytes().to_vec()).collect::<HashSet<Vec<u8>>>();
-            con.transaction(|con| {
-                // Find existing chain blocks and remove them from the insert queue
-                blocks::dsl::blocks
-                    .select(blocks::hash)
-                    .filter(blocks::hash.eq_any(&chain_hashes)
-                        .and(blocks::is_chain_block.eq(true)))
-                    .load::<Vec<u8>>(con)
-                    .expect("Commit transactions to database FAILED").iter()
-                    .for_each(|b| {
-                        chain_hashes.remove(b);
-                    });
-                //Upsert blocks in case a conflicting block was persisted
-                rows_affected = insert_into(blocks::dsl::blocks)
-                    .values(chain_hashes.iter().map(|h| Block::new(h.clone(), true)).collect::<Vec<Block>>())
-                    .on_conflict(blocks::hash)
-                    .do_update()
-                    .set(blocks::is_chain_block.eq(true))
-                    .execute(con)
-                    .expect("Commit added chain blocks to database FAILED");
-                Ok::<_, Error>(())
-            }).expect("Commit added chain blocks to database FAILED");
-            info!("Committed {} added chain blocks to database", rows_affected);
-        }
+        update_chain_blocks(response.added_chain_block_hashes, response.removed_chain_block_hashes, con);
 
         // Save checkpoint as the last accepting_block_hash
         if !accepted_queue.is_empty() && SystemTime::now().duration_since(checkpoint_hash_last_saved).unwrap().as_secs() > 60 {
@@ -124,4 +99,46 @@ pub async fn process_virtual_chain(checkpoint_hash: String,
         }
         sleep(Duration::from_secs(1)).await;
     }
+}
+
+fn update_chain_blocks(added_hashes: Vec<RpcHash>, removed_hashes: Vec<RpcHash>, con: &mut PooledConnection<ConnectionManager<PgConnection>>) {
+    info!("Received {} added chain blocks", added_hashes.len());
+    trace!("Added chain blocks: \n{:#?}", added_hashes);
+    info!("Received {} removed chain blocks", removed_hashes.len());
+    trace!("Removed chain blocks: \n{:#?}", removed_hashes);
+    let mut rows_affected = 0;
+    let mut updated_chain_blocks = HashMap::with_capacity(added_hashes.len() + removed_hashes.len());
+
+    // Join added and removed blocks to only require one exists and one insert query
+    for added_hash in added_hashes {
+        updated_chain_blocks.insert(added_hash.as_bytes().to_vec(), true);
+    }
+    for removed_hash in removed_hashes {
+        updated_chain_blocks.insert(removed_hash.as_bytes().to_vec(), false);
+    }
+    if !updated_chain_blocks.is_empty() {
+        con.transaction(|con| {
+            // Find existing and remove them from the insert queue
+            blocks::dsl::blocks
+                .select((blocks::hash, blocks::is_chain_block))
+                .filter(blocks::hash.eq_any(updated_chain_blocks.keys()))
+                .load::<(Vec<u8>, bool)>(con)
+                .expect("Select chain blocks from database FAILED").iter()
+                .for_each(|(b, is_cb)| {
+                    if updated_chain_blocks.get(b).unwrap() == is_cb {
+                        updated_chain_blocks.remove(b); // is_chain_block is already set to the same value in db
+                    }
+                });
+            // Save changed rows
+            rows_affected = insert_into(blocks::dsl::blocks)
+                .values(updated_chain_blocks.iter().map(|(h, is_cb)| Block::new(h.clone(), *is_cb)).collect::<Vec<Block>>())
+                .on_conflict(blocks::hash)
+                .do_update()
+                .set(blocks::is_chain_block.eq(excluded(blocks::is_chain_block))) // Upsert guarantees consistency when rows where inserted since the select
+                .execute(con)
+                .expect("Commit updated chain blocks to database FAILED");
+            Ok::<_, Error>(())
+        }).expect("Commit updated chain blocks to database FAILED");
+    }
+    info!("Committed {} updated chain blocks to database", rows_affected);
 }
