129aa15489b950de8aa70f86cd3fbac2989c1967
supertypo <suprtypo@pm.me>
2024-12-22 20:30:53 +0100

    docker: Force origin/$TAG

--- docker/build.sh
+++ docker/build.sh
@@ -18,7 +18,7 @@ fi
 echo "============================================================="
 echo " Pulling $REPO_URL"
 echo "============================================================="
-(cd "$REPO_DIR" && git fetch && git checkout $TAG && git reset --hard $TAG)
+(cd "$REPO_DIR" && git fetch && git checkout $TAG && git reset --hard origin/$TAG)
 
 tag=$(cd "$REPO_DIR" && git log -n1 --format="%cs.%h")
 
