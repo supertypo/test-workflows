2fdc8b1ebd2a3b72b9976cfea541515798d80e4f
supertypo <suprtypo@pm.me>
2024-12-22 20:30:53 +0100

    Removed unused index: transactions_outputs (script_public_key_address);

--- database/migrations/schema/up.sql
+++ database/migrations/schema/up.sql
@@ -95,7 +95,6 @@ CREATE TABLE transactions_outputs
     PRIMARY KEY (transaction_id, index)
 );
 CREATE INDEX ON transactions_outputs (transaction_id);
-CREATE INDEX ON transactions_outputs (script_public_key_address);
 
 
 CREATE TABLE addresses_transactions
