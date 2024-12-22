7f7b1794154a3ca4e83d7c2ecc1bd5dfaad48422
supertypo <suprtypo@pm.me>
2024-12-22 20:30:54 +0100

    v2_to_v3.sql: Corrected schema_version
    # Conflicts:
    #	database/migrations/schema/up.sql
    #	database/migrations/schema/v1_to_v2.sql
    #	database/migrations/schema/v2_to_v3.sql
    #	database/src/client/client.rs

--- database/migrations/schema/v2_to_v3.sql
+++ database/migrations/schema/v2_to_v3.sql
@@ -10,4 +10,4 @@ DROP INDEX IF EXISTS transactions_outputs_transaction_id_idx;
 DROP INDEX IF EXISTS addresses_transactions_address_idx;
 
 -- Update schema_version
-UPDATE vars SET value = '2' WHERE key = 'schema_version';
+UPDATE vars SET value = '3' WHERE key = 'schema_version';
