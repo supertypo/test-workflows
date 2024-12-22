a553ee880e9dd6f9e9120df83dcfab69ec16fd6e
supertypo <suprtypo@pm.me>
2024-12-22 20:30:52 +0100

    Require 0 blocks inserted to consider empty acceptance data before VCP

--- src/blocks/insert_blocks.rs
+++ src/blocks/insert_blocks.rs
@@ -25,7 +25,7 @@ pub async fn insert_blocks(running: Arc<AtomicBool>,
                            start_vcp: Arc<AtomicBool>,
                            db_blocks_queue: Arc<ArrayQueue<(Block, Vec<Vec<u8>>)>>,
                            db_pool: Pool<ConnectionManager<PgConnection>>) {
-    const EMPTY_DELETES_BEFORE_VCP: i32 = 10;
+    const NOOP_DELETES_BEFORE_VCP: i32 = 10;
     const CHECKPOINT_SAVE_INTERVAL: u64 = 60;
     const CHECKPOINT_WARN_AFTER: u64 = 5 * CHECKPOINT_SAVE_INTERVAL;
     let max_queue_size = min((1000f64 * buffer_size) as usize, 3500); // ~3500 is the max batch size db supports
@@ -38,7 +38,7 @@ pub async fn insert_blocks(running: Arc<AtomicBool>,
     let mut checkpoint_hash_tx_expected_count = 0;
     let mut checkpoint_last_saved = Instant::now();
     let mut last_commit_time = Instant::now();
-    let mut empty_delete_count = 0;
+    let mut noop_delete_count = 0;
 
     while running.load(Ordering::Relaxed) {
         let block_tuple = db_blocks_queue.pop();
@@ -59,6 +59,12 @@ pub async fn insert_blocks(running: Arc<AtomicBool>,
             let mut blocks_inserted = 0;
             let con = &mut db_pool.get().expect("Database connection FAILED");
             con.transaction(|con| {
+                blocks_inserted = insert_into(blocks::dsl::blocks)
+                    .values(Vec::from_iter(insert_queue.iter()))
+                    .on_conflict_do_nothing()
+                    .execute(con)
+                    .expect("Commit blocks FAILED");
+
                 if !vcp_started {
                     let delete_block_hashes: Vec<Vec<u8>> = insert_queue.iter().map(|b| b.hash.clone()).collect();
                     ta_cleared = delete(transactions_acceptances::dsl::transactions_acceptances)
@@ -69,23 +75,15 @@ pub async fn insert_blocks(running: Arc<AtomicBool>,
                         .filter(chain_blocks::block_hash.eq_any(&delete_block_hashes))
                         .execute(con)
                         .expect("Commit removed chain blocks FAILED");
-                    if cb_cleared == 0 && ta_cleared == 0 {
-                        empty_delete_count += 1;
-                    } else {
-                        empty_delete_count = 0;
-                    }
+                    if blocks_inserted > 0 && cb_cleared == 0 && ta_cleared == 0 { noop_delete_count += 1; } else { noop_delete_count = 0; }
                 }
-                if !vcp_started && empty_delete_count > EMPTY_DELETES_BEFORE_VCP {
+
+                if !vcp_started && noop_delete_count >= NOOP_DELETES_BEFORE_VCP {
                     info!("End of previous run reached, notifying virtual chain processor");
                     start_vcp.store(true, Ordering::Relaxed);
                     vcp_started = true;
                     checkpoint_last_saved = checkpoint_last_saved.add(Duration::from_secs(CHECKPOINT_SAVE_INTERVAL));
                 }
-                blocks_inserted = insert_into(blocks::dsl::blocks)
-                    .values(Vec::from_iter(insert_queue.iter()))
-                    .on_conflict_do_nothing()
-                    .execute(con)
-                    .expect("Commit blocks FAILED");
                 Ok::<_, Error>(())
             }).expect("Commit blocks FAILED");
             last_commit_time = Instant::now();
