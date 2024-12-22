45de87be6cc75e76f27daccecc30eace05257e72
supertypo <suprtypo@pm.me>
2024-12-22 20:30:53 +0100

    up.sql: Removed unused trigger function

--- migrations/00000000000010_tables/up.sql
+++ migrations/00000000000010_tables/up.sql
@@ -107,28 +107,3 @@ CREATE TABLE addresses_transactions
 CREATE INDEX ON addresses_transactions (address);
 CREATE INDEX ON addresses_transactions (transaction_id);
 CREATE INDEX ON addresses_transactions (block_time DESC);
-
-
-CREATE FUNCTION update_adresses_transactions()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    INSERT INTO addresses_transactions (address, transaction_id, block_time)
-    SELECT o.script_public_key_address,
-           i.transaction_id,
-           t.block_time
-    FROM transactions_inputs i
-             JOIN transactions t ON t.transaction_id = i.transaction_id
-             JOIN transactions_outputs o ON o.transaction_id = i.previous_outpoint_hash AND o.index = i.previous_outpoint_index
-    WHERE i.transaction_id = NEW.transaction_id
-    ON CONFLICT (address, transaction_id) DO NOTHING;
-
-    RETURN NEW; -- Must return NEW to proceed with the insert into blocks_transactions
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trigger_before_insert_blocks_transactions
-    BEFORE INSERT
-    ON blocks_transactions
-    FOR EACH ROW
-EXECUTE FUNCTION update_adresses_transactions();
