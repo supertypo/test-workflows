2ee4cacd5ae2ce578d9d78517b5c7a1e4fa44c64
supertypo <suprtypo@pm.me>
2025-02-06 01:35:05 +0100

    Added auto-generated version using vergen-git2

---
+++ cli/build.rs
@@ -0,0 +1,12 @@
+use vergen_git2::{Emitter, Git2Builder};
+
+fn main() {
+    let git2 = Git2Builder::default()
+        .branch(true)
+        .commit_date(true)
+        .sha(true)
+        .describe(true, false, None)
+        .build()
+        .unwrap();
+    Emitter::default().add_instructions(&git2).unwrap().emit().unwrap();
+}
